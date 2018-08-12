ef6fc2424a332ba83d468290d359345234b36354
Frank Li <frankli0324@hotmail.com>
2018-08-12 20:38:19 +0800

    man

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-	
+	//happy hacking!
+	//please open this project with Visual Studio
+	//    (latest version) for (maybe) a fine experience
+	//currently supported functions:
+	//1/  remote connection to a host, equivalent to the
+	//    Tubes module in the pwntools library
+	//2/  Big Integer Calculation
+	//3/  Serveral Cipers
+	//4/  Serveral Encodings
+	//5/  Logger support, keep yourself informed on which
+	//    step your kit is working on
+
+	//TODO list:
+	//1/  
 }
